d37db7d155d7aedc2f04ef31732466ee4487c401
KirkosChainz <nicklee58103@gmail.com>
2025-10-07 23:08:54 -0500

    Update products.js

--- products.js
+++ products.js
@@ -1,8 +1,9 @@
 const products = [
-    { name: "Product 1", price: 10, img: "images/product1.jpg" },
-    { name: "Product 2", price: 15, img: "images/product2.jpg" },
-    { name: "Product 3", price: 12, img: "images/product3.jpg" },
+    { name: "Product 1", price: 20, img: "images/product1.jpg" },
+    { name: "Product 2", price: 20, img: "images/product2.jpg" },
+    { name: "Product 3", price: 20, img: "images/product3.jpg" },
     { name: "Product 4", price: 20, img: "images/product4.jpg" },
-    { name: "Product 5", price: 8, img: "images/product5.jpg" },
-    { name: "Product 6", price: 25, img: "images/product6.jpg" }
+    { name: "Product 5", price: 20, img: "images/product5.jpg" },
+    { name: "Product 6", price: 35, img: "images/product6.jpg" }
 ];
+
